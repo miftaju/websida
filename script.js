87282d435956eb109676795bf055565c451dd99f
Seif Said <seif-diya@hotmail.com>
2024-06-20 14:28:54 +0200

    javascript script that take input from contact form + cleaned up some html code in contact.html

--- script.js
+++ script.js
@@ -11,16 +11,41 @@ function loadDoc() {
 function myFunction(xml) {
   var i;
   var xmlDoc = xml.responseXML;
+  var index = 'Midnight Rain'
   var table="<tr><th>Author</th><th>Title</th><th>Genre</th></tr>";
   var x = xmlDoc.getElementsByTagName("book");
-  for (i = 0; i <x.length; i++) { 
-    table += "<tr><td>" +
-    x[i].getElementsByTagName("author")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("genre")[0].childNodes[0].nodeValue +
-    "</td></tr>";
-  }
+  for (i = 0; i <x.length; i++) 
+    { 
+    table += "<tr><td>" + x[i].getElementsByTagName("author")[0].childNodes[0].nodeValue + "</td><td>" + x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue + "</td><td>" + x[i].getElementsByTagName("genre")[0].childNodes[0].nodeValue + "</td></tr>";
+    }
   document.getElementById("demo").innerHTML = table;
+}
+
+function jclick() 
+{
+  var fname = document.getElementById('fname');
+  var lname = document.getElementById('lname');
+  var message = document.getElementById('message');
+  var errorMessage = '';
+  if (fname.value === '' || lname.value === '')
+    {
+      errorMessage += 'Please input your Name\n';
+    }
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if(!emailRegex.test(email.value)) 
+    {
+      errorMessage += 'Invalid email!\n';
+    }
+
+  if (message.value === '') 
+    {
+      errorMessage += 'You need to input a message to submit a contact form\n'
+    }
+
+  if (errorMessage !== '') 
+    {
+      alert(errorMessage);
+      return false;
+    }
+  return true;
 }
